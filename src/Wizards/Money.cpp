b2bf93227a988cb6434fb83231500a7c404a697e
Aaron3154518 <aaoren210@gmail.com>
2022-08-30 11:28:55 -0400

    Animated crystal shards

--- src/Wizards/Money.cpp
+++ src/Wizards/Money.cpp
@@ -3,20 +3,28 @@
 namespace Money {
 const RenderDataPtr& GetMoneyIcon(const ParameterSystem::ValueParam& param) {
     static RenderDataPtr DEFAULT, CRYSTAL_MAGIC, CRYSTAL_SHARDS, CATALYST_MAGIC;
-    static TimerObservable::SubscriptionPtr CRYS_MAG_SUB;
+    static TimerObservable::SubscriptionPtr CRYS_MAG_SUB, CRYS_SHARD_SUB;
     if (!CRYSTAL_MAGIC) {
+        auto timerObs = ServiceSystem::Get<TimerService, TimerObservable>();
+
         CRYSTAL_MAGIC = std::make_shared<RenderData>();
         CRYSTAL_MAGIC->set("res/money/crystal_magic.png", 6);
-        CRYS_MAG_SUB =
-            ServiceSystem::Get<TimerService, TimerObservable>()->subscribe(
-                [](Timer& t) {
-                    CRYSTAL_MAGIC->nextFrame();
-                    return true;
-                },
-                Timer(150));
+        CRYS_MAG_SUB = timerObs->subscribe(
+            [](Timer& t) {
+                CRYSTAL_MAGIC->nextFrame();
+                return true;
+            },
+            Timer(150));
 
         CRYSTAL_SHARDS = std::make_shared<RenderData>();
-        CRYSTAL_SHARDS->set("res/money/crystal_shard.png");
+        CRYSTAL_SHARDS->set("res/money/crystal_shard.png", 9);
+        CRYS_SHARD_SUB = timerObs->subscribe(
+            [](Timer& t) {
+                CRYSTAL_SHARDS->nextFrame();
+                t.length = CRYSTAL_SHARDS->getFrame() == 0 ? 200 : 100;
+                return true;
+            },
+            Timer(100));
 
         CATALYST_MAGIC = std::make_shared<RenderData>();
         CATALYST_MAGIC->set("res/wizards/catalyst.png");
